106c54b558c167fce707ec158ab79b33c12cb36f
Suprabath kondapally <suprabathkondapally@gmail.com>
2022-09-10 13:36:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const displayEntries = () => {
                         <th class='px-4 py-2 '>Email </th>
                         <th class='px-4 py-2 '>Password </th>
                         <th class='px-4 py-2 '>Dob </th>
-                        <th class='px-4 py-2 '>Accepted terms </th>
+                        <th class='px-4 py-2 '>Accepted terms? </th>
                         </tr>${tableEntries}
                     </table>`;
     let details = document.getElementById('user_entries');
